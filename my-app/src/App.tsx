705e9682d2d5b7d94d197462853035aeaed68104
yi-ting <yumy19990628@gmail.com>
2023-03-15 13:13:38 +0800

    Add: input value

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
   // const answer:string[] = ['M', 'A', 'G', 'I', 'C'];
   // const inputValue = ['M', 'A', 'G', 'B', 'B'];
   // const currentRow:number = 1;
   // const state:string[] = ['gray', 'green', 'yellow', 'active'];
-  const allInputData = table(6, 5);
-  const [inputData, setInputData] = useState(allInputData);
+  // const allInputData = table(6, 5);
+  // const [inputData, setInputData] = useState(allInputData);
 
   const stateClassName: { [key: string]: string } = {
     gray: 'text-white border-[#787c7e] bg-[#787c7e]',
@@ -15,93 +15,94 @@ function App() {
     active: 'bg-white border-black',
   };
 
-  // const allInputData = [
-  //   [
-  //     { letter: 'M', state: 'green' },
-  //     { letter: 'A', state: 'green' },
-  //     { letter: 'I', state: 'yellow' },
-  //     { letter: 'Z', state: 'gray' },
-  //     { letter: 'C', state: 'green' },
-  //   ],
-  //   [
-  //     { letter: 'M', state: 'active' },
-  //     { letter: 'A', state: 'active' },
-  //     { letter: 'G', state: 'active' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //   ],
-  //   [
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //   ],
-  //   [
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //   ],
-  //   [
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //   ],
-  //   [
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //     { letter: '', state: '' },
-  //   ],
-  // ];
-  function table(colLength: number, rowLength: number) {
-    const col = [];
-    for (let i = 0; i < rowLength; i++) {
-      col.push({ letter: '', state: '' });
-    }
-    const table = [];
-    for (let i = 0; i < colLength; i++) {
-      table.push(col);
-    }
-    return table;
-  }
+  const [allInputData, setAllInputData] = useState([
+    [
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+    ],
+    [
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+    ],
+    [
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+    ],
+    [
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+    ],
+    [
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+    ],
+    [
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+      { letter: '', status: '' },
+    ],
+  ]);
+  // function table(colLength: number, rowLength: number) {
+  //   const col = [];
+  //   for (let i = 0; i < rowLength; i++) {
+  //     col.push({ letter: '', status: '' });
+  //   }
+  //   const table = [];
+  //   for (let i = 0; i < colLength; i++) {
+  //     table.push(col);
+  //   }
+  //   return table;
+  // }
+  const inputRefs = useRef([]);
 
-  function handleInputChange(i: number, j: number, e: any) {
-    const newValue = e.target.value;
-    setInputData((prev) => {
-      const newInputData = [...prev];
-      console.log(newInputData);
-      newInputData[i][j] = {
-        ...newInputData[i][j],
-        letter: newValue,
-      };
-      return newInputData;
-    });
-  }
+  const handleInputChange = (i, j, input, event) => {
+    const newData = [...allInputData];
+    newData[i][j].letter = input.toUpperCase();
+    console.log(newData);
+    setAllInputData(newData);
+  };
+  console.log(allInputData);
 
-  console.log(inputData);
   return (
     <div className="box-border ">
       <div className="text-3xl text-center flex justify-center items-center h-20 border-b border-gray-600">
-        Yumy's Wordle
+        ε(*´･∀･｀)зﾞ Yumy's Wordle ε(´･∀･｀*)зﾞ
       </div>
       <div className="mb-14 mt-14 flex flex-col h-[400px] w-[330px] justify-between mr-auto ml-auto">
-        {inputData.map((item, i) => (
+        {allInputData.map((item, i) => (
           <div key={i} className="flex flex-wrap justify-between">
             {item.map((letterData, j) => (
               <input
                 key={j}
                 type="text"
                 className={`w-[62px] h-[62px] border-2 text-2xl text-center ${
-                  stateClassName[letterData.state]
+                  stateClassName[letterData.status]
                 }`}
                 value={letterData.letter}
-                onChange={(e) => handleInputChange(i, j, e)}
+                ref={(el) => {
+                  if (!inputRefs.current[i]) {
+                    inputRefs.current[i] = [];
+                  }
+                  inputRefs.current[i][j] = el;
+                }}
+                onChange={(e) => handleInputChange(i, j, e.target.value, e)}
               />
             ))}
           </div>
